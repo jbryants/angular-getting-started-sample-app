1401ff16f66d8bc9dfca8d821b2bea1b4cd28df4
jbryants <jbryants19d@gmail.com>
2020-09-01 12:58:08 +0530

    Part 1 done: Components and template syntax, how the component class and template interact, and how components communicate with each other.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,12 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
+   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
